60905f62e8c4a0909bcf84f5c78b2106a61c6a9d
Sasha Limarev <slimarev@outlook.com>
2023-05-12 13:31:42 +0300

    Fixed linter error.

--- src/app/services/app-language.service.ts
+++ src/app/services/app-language.service.ts
@@ -4,7 +4,7 @@ const SUPPORTED_RTL_LOCALES_SET = new Set(["ar", "he"]);
 
 @Injectable({providedIn: "root"})
 export class AppLanguageService {
-    private localeId = inject(LOCALE_ID);
+    private readonly localeId = inject(LOCALE_ID);
 
     // TODO SASHA: THIS CAN BE A DI TOKEN WITH A FACTORY INSTEAD.
     public getAppTextDirection() {
